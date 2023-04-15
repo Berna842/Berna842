db6229ba91cda75909688d883c6814bb9fba780e
Berna842 <rodriguezquezadabernardo@gmail.com>
2023-04-15 13:01:28 -0700

    Machine Learingn test 1

--- Machine Learning/test1.py
+++ Machine Learning/test1.py
@@ -1,16 +1,41 @@
-from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
-import pandas as pd
+from sklearn.model_selection import train_test_split
 
-dataset = datasets.load_boston()
+np.random.seed(42)
 
-objetivo = dataset['target']
-independientes = dataset['data']
+n_samples, n_features = 200, 50
+X = np.random.randn(n_samples, n_features)
+true_coef = 3 * np.random.randn(n_features)
+# Threshold coefficients to render them non-negative
+true_coef[true_coef < 0] = 0
+y = np.dot(X, true_coef)
 
-modelo = LinearRegression()
+# Add some noise
+y += 5 * np.random.normal(size=(n_samples,))
 
-modelo.fit(X=independientes, y=objetivo)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
-predicciones = modelo.predict(independientes)
-for y, y_pred in list(zip(objetivo, predicciones)) [:5]:
-	print("Valor Real: {:.3f} Valor Estimado: {:.5f}".format(y, y_pred))
+reg_nnls = LinearRegression(positive=True)
+y_pred_nnls = reg_nnls.fit(X_train, y_train).predict(X_test)
+r2_score_nnls = r2_score(y_test, y_pred_nnls)
+print("NNLS R2 score", r2_score_nnls)
+
+reg_ols = LinearRegression()
+y_pred_ols = reg_ols.fit(X_train, y_train).predict(X_test)
+r2_score_ols = r2_score(y_test, y_pred_ols)
+print("OLS R2 score", r2_score_ols)
+
+fig, ax = plt.subplots()
+ax.plot(reg_ols.coef_, reg_nnls.coef_, linewidth=0, marker=".")
+
+low_x, high_x = ax.get_xlim()
+low_y, high_y = ax.get_ylim()
+low = max(low_x, low_y)
+high = min(high_x, high_y)
+ax.plot([low, high], [low, high], ls="--", c=".3", alpha=0.5)
+ax.set_xlabel("OLS regression coefficients", fontweight="bold")
+ax.set_ylabel("NNLS regression coefficients", fontweight="bold")
+plt.show()
